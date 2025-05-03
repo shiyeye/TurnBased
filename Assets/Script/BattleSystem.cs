80a3a3bf31ccecaefd3b012ee31f4614c57ae095
shiyeye <1935780829@qq.com>
2025-05-03 20:06:52 +0800

    upload new unity project

--- Assets/Script/BattleSystem.cs
+++ Assets/Script/BattleSystem.cs
@@ -52,9 +52,94 @@ public class BattleSystem : MonoBehaviour
         PlayerTurn();
     }
 
+    IEnumerator PlayerAttack()
+    {
+        bool isDead = enemyUnit.TakeDamage(playerUnit.damage);
+
+        enemyHud.SetHP(enemyUnit.currentHP);
+        dialogueText.text = "The attack was successful! ";
+
+        yield return new WaitForSeconds(2f); 
+
+        if(isDead)
+        {
+            state = BattleState.WON;
+            EndBattle();
+        }
+        else
+        {
+            state = BattleState.ENEMYTURN;
+            StartCoroutine(EnemyTurn());
+        }
+    }
+
+    IEnumerator EnemyTurn()
+    {
+        dialogueText.text = enemyUnit.unitName + " attacks!";
+
+        yield return new WaitForSeconds(1f);
+
+        bool isDead = playerUnit.TakeDamage(enemyUnit.damage);
+
+        playerHud.SetHP(playerUnit.currentHP);
+
+        yield return new WaitForSeconds(1f);
+
+        if (isDead)
+        {
+            state = BattleState.LOST;
+            EndBattle();
+        }
+        else
+        {
+            state = BattleState.PLAYTURN;
+            PlayerTurn();
+        }
+    }
+
+    void EndBattle()
+    {
+        if (state == BattleState.WON)
+        {
+            dialogueText.text = "You won the battle!";
+        }
+        else if (state == BattleState.LOST)
+        {
+            dialogueText.text = "You lost the battle!";
+        }
+    }
+
     void PlayerTurn()
     {
         dialogueText.text = "What is your choice?";
+    }
+    
+    IEnumerator PlayerPropButton()
+    {
+        playerUnit.Prop(50);
+
+        playerHud.SetHP(playerUnit.currentHP);
+        dialogueText.text = "You used a prop!";
+
+        yield return new WaitForSeconds(2f);
+
+        state = BattleState.ENEMYTURN;
+        StartCoroutine(EnemyTurn());
+    }
+    public void OnAttackButton()
+    {
+        if (state != BattleState.PLAYTURN)
+            return;
+
+        StartCoroutine(PlayerAttack());
+    }
+
+    public void OnPropButton()
+    {
+        if (state != BattleState.PLAYTURN)
+            return;
+        
+        StartCoroutine(PlayerPropButton());
     }
 
     // Update is called once per frame
--- Assets/Script/Unit.cs
+++ Assets/Script/Unit.cs
@@ -10,5 +10,28 @@ public class Unit : MonoBehaviour
     public int currentHP;
     public int damage;
 
-    
+    public bool TakeDamage(int damage)
+    {
+        currentHP -= damage;
+        if (currentHP <= 0)
+            return true; // Unit is dead
+        else
+            return false; // Unit is still alive
+    }
+
+    public void Prop(int prop)
+    {
+        currentHP += prop; // Assuming prop has an effectValue property
+        if (currentHP > maxHP)
+            currentHP = maxHP; // Ensure HP does not exceed maxHP
+    }
+
+    public void Heal(int heal)
+    {
+        currentHP += heal;
+        if (currentHP > maxHP)
+        {
+            currentHP = maxHP;
+        }
+    }
 }
